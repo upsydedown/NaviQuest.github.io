dca5a9800133feb737cd549d0a333139f1b3c05b
upsydedown <chintansenapati@gmail.com>
2023-02-01 04:58:41 +0530

    Adding Search History logic

--- src/Map.js
+++ src/Map.js
@@ -5,27 +5,29 @@ import L from 'leaflet'
 
 const icon = L.icon({
     iconUrl: "./images/logo.gif",
-    iconSize: (50,50),
+    iconSize: (50, 50),
 });
 
 const position = [51.505, -0.09]
-
 const Map = () => {
+
     return (
-        <MapContainer center={position} 
-        zoom={13} 
-        scrollWheelZoom={true} style={{ height: '100%',borderRadius: '15px', width: '100%', margin: '0.5rem 0.5rem 0.5rem 0.5rem'}}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=XmHBbvksCF8v4vwMs38M"
-            />
-            <Marker position={position} icon={icon}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <>
+            <MapContainer center={position}
+                zoom={13}
+                scrollWheelZoom={true} style={{ zIndex: '1', height: '100%', width: '100%', borderTopLeftRadius: '15px', borderTopRightRadius: '15px' }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=XmHBbvksCF8v4vwMs38M"
+                />
+                <Marker position={position} icon={icon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </>
     );
 };
 
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,24 +1,111 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "./Searchbox.css"
+import { polygon } from 'leaflet';
+
+const MAP_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+
+const params = {
+    q: '',
+    format: 'json',
+    addressdetails: 'addressdetails',
+
+}
 
 const Searchbox = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchHistory, setSearchHistory] = useState([]);
+    const [filteredHistory, setFilteredHistory] = useState([]);
+    const [showHistory, setShowHistory] = useState(false);
+
+    useEffect(() => {
+        const history = localStorage.getItem('searchHistory');
+        if (history) {
+            setSearchHistory(JSON.parse(history));
+        }
+    }, []);
+
+    const handleSearch = event => {
+        setSearchValue(event.target.value);
+        if (!searchValue) {
+            setFilteredHistory(searchHistory);
+        } else { //Add link here
+            setFilteredHistory(
+                searchHistory.filter(item =>
+                    item.toLowerCase().includes(searchValue.toLowerCase())
+                )
+            );
+        }
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const updatedSearchHistory = [...searchHistory, searchValue];
+        localStorage.setItem('searchHistory', JSON.stringify(updatedSearchHistory));
+        setSearchHistory(updatedSearchHistory);
+        setFilteredHistory(updatedSearchHistory);
+        setSearchValue('');
+    };
+
+    const handleInputFocus = () => {
+        setShowHistory(true);
+    };
+
+    const handleInputBlur = () => {
+        setShowHistory(false);
+    };
+
+
     return (
         <>
             <div className="Searchbox">
-                <div className="Searchbar">
-                    <input type="text" submi placeholder='Search Here' />
-                    <span id='Search_btn'><button>Search</button></span>
-                </div>
-
-                {/* <div className="Search_list">
-                    <ul>
-                        <li>Hello</li>
-                    </ul>
-                </div> */}
-
+                <form onSubmit={handleSubmit}>
+                    <div className="Searchbar" >
+                        <input type="text"
+                            placeholder='Search Here'
+                            value={searchValue}
+                            onChange={handleSearch}
+                            onFocus={handleInputFocus}
+                            onBlur={handleInputBlur} />
+                        
+                        <span id='Search_btn'><button onClick={() => {
+                            
+                            const params = {
+                                q: searchValue,
+                                format: 'json',
+                                addressdetails: '1',
+                                polygon_geojson: 1,
+                            };
+                           
+                            const queryString = new URLSearchParams(params).toString();
+                            
+                            const requestOptions = {
+                                method: 'GET',
+                                redirect: 'follow',
+                            };
+                            
+                            fetch(`${MAP_BASE_URL}${queryString}`, requestOptions)
+                                .then((response) => response.text())
+                                .then((result) => {
+                                    console.log(JSON.parse(result));
+                                }).catch((error) => console.log("error: ", error));
+                        }}>Search</button></span>
+                    
+                    </div>
+                    
+                    <div className="Search_list">
+                        {filteredHistory.length > 0 && (
+                            <ul>
+                                {filteredHistory.reverse().map((item, index) => (
+                                    <li key={index}><img style={{ width: '1.1rem' }} src="./images/list.gif" alt="" srcset="" /> {item}</li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </form>
             </div>
         </>
     )
 }
 
 export default Searchbox;
+
